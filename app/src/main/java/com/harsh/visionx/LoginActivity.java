bf6014b3b48a3dac1c624107384c77d2ded97789
HarshpancholiX <harshpancholi518@gmail.com>
2026-08-14 18:29:10 +0530

    New App

--- app/src/main/java/com/harsh/visionx/LoginActivity.java
+++ app/src/main/java/com/harsh/visionx/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.harsh.visionx;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Toast.makeText(LoginActivity.this, "jai Tango Gang", Toast.LENGTH_SHORT).show();
     }
 }
